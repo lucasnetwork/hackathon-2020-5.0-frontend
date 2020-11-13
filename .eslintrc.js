85b563faf21e67d62c2fd34611f4282430f6b964
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-11-13 18:10:32 -0300

    feat(init): add index file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  env: {
-    es2021: true,
-  },
-  extends: ["plugin:react/recommended", "airbnb", "prettier", "prettier/react"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react", "prettier"],
-  rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["warn", { extensions: ["js", "jsx"] }],
-  },
+	env: {
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react', 'prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+		'react/jsx-filename-extension': ['warn', { extensions: ['js', 'jsx'] }],
+	},
 };
