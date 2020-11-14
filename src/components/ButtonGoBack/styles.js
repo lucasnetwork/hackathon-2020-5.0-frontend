1ca0b169d125468c6134360635d45615f5b14f58
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-11-14 20:36:41 -0300

    feat(component): remove margin of ButtonGoBack component

--- src/components/ButtonGoBack/styles.js
+++ src/components/ButtonGoBack/styles.js
@@ -1,15 +1,11 @@
 import styled from 'styled-components/native';
 
-import Contants from 'expo-constants';
-
 export default styled.TouchableOpacity`
 	width: 40px;
 	height: 40px;
 	border-radius: 50px;
 	background: #ffffff;
 	box-shadow: 6px 6px 10px rgba(0, 0, 0, 0.06);
-	margin-top: ${`${Contants.statusBarHeight + 12}px`};
-	margin-left: 16px;
 	align-items: center;
 	justify-content: center;
 `;
