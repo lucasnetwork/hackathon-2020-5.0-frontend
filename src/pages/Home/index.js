3fd2845d9f6c48028fc58b184166979ea994e4fa
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-11-15 12:54:43 -0300

    feat(pages): update Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Image, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 import Container, {
 	TitleContainer,
 	Title,
 	TitleInvoice,
 	InvoiceList,
-	DateText,
-	Card,
 	Containerclub,
 	InvoiceContainer,
-	TitleCard,
+	TextCard,
+	TitleClub,
 } from './styles';
-
 import InputSearch from '../../components/InputSearch';
 import giftBoxImage from '../../images/giftBox.png';
+import handShakeImage from '../../images/handShake/handshake.png';
 import ButtonChat from '../../components/ButtonChat';
 import InvoiceCard from './components/InvoiceCard';
+import Card from '../../components/Card';
 
 const Home = () => {
 	const navigate = useNavigation();
@@ -24,41 +25,67 @@ const Home = () => {
 	function goToChatBot() {
 		navigate.navigate('ChatBot');
 	}
+	function goToIndicateFriend() {
+		navigate.navigate('IndicateFriend');
+	}
+
 	return (
-		<Container>
+		<>
 			<ButtonChat onPress={goToChatBot} />
-			<TitleContainer>
-				<Title>Olá, Carlos!</Title>
-				<InputSearch />
-			</TitleContainer>
-			<InvoiceContainer>
-				<TitleInvoice>Suas Faturas</TitleInvoice>
-				<InvoiceList horizontal>
-					<InvoiceCard
-						price="343"
-						points="21"
-						state="paid"
-						dateVenc="08/10/2020"
-					/>
-					<InvoiceCard
-						price="343"
-						points="21"
-						state="wait"
-						dateVenc="08/10/2020"
-					/>
-				</InvoiceList>
-			</InvoiceContainer>
-			<Containerclub>
-				<Title>CPFL Clube</Title>
-				<Card>
-					<Image source={giftBoxImage} />
-					<View>
-						<DateText>Saldo total</DateText>
-						<TitleCard>450 pontos</TitleCard>
-					</View>
-				</Card>
-			</Containerclub>
-		</Container>
+			<Container>
+				<View style={{ flex: 1, paddingBottom: 32 }}>
+					<TitleContainer>
+						<Title>Olá, Carlos!</Title>
+						<InputSearch />
+					</TitleContainer>
+					<InvoiceContainer>
+						<TitleInvoice>Suas Faturas</TitleInvoice>
+						<InvoiceList horizontal>
+							<InvoiceCard
+								price="343"
+								points="21"
+								state="paid"
+								dateVenc="08/10/2020"
+							/>
+							<InvoiceCard
+								price="343"
+								points="21"
+								state="wait"
+								dateVenc="08/10/2020"
+							/>
+						</InvoiceList>
+					</InvoiceContainer>
+					<Containerclub>
+						<TitleClub>CPFL Clube</TitleClub>
+						<Card>
+							<Image source={giftBoxImage} />
+							<View>
+								<TextCard small>Saldo total</TextCard>
+								<TextCard tertiary>450 pontos</TextCard>
+							</View>
+							<Ionicons
+								name="md-arrow-round-forward"
+								size={30}
+								color="#3F3F3F"
+							/>
+						</Card>
+					</Containerclub>
+					<Containerclub>
+						<TitleClub>Ganhe pontos</TitleClub>
+						<Card secondary onPress={goToIndicateFriend}>
+							<Image source={handShakeImage} />
+							<View>
+								<TextCard secondary small>
+									Indique um amigo e ganhe agoral
+								</TextCard>
+								<TextCard secondary>200 pontos</TextCard>
+							</View>
+							<Ionicons name="md-arrow-round-forward" size={30} color="#fff" />
+						</Card>
+					</Containerclub>
+				</View>
+			</Container>
+		</>
 	);
 };
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,15 +1,13 @@
 import styled, { css } from 'styled-components/native';
 
-export default styled.View`
+export default styled.ScrollView`
 	flex: 1;
-	padding: 16px;
-	align-items: center;
-	justify-content: center;
 	padding-top: 50px;
 `;
 
 export const TitleContainer = styled.View`
 	margin-bottom: 32px;
+	padding: 0 16px;
 `;
 
 export const Title = styled.Text`
@@ -18,7 +16,9 @@ export const Title = styled.Text`
 	font-weight: bold;
 `;
 
-export const InvoiceContainer = styled.View``;
+export const InvoiceContainer = styled.View`
+	padding: 0 16px;
+`;
 
 export const TitleInvoice = styled.Text`
 	font-size: 16px;
@@ -28,44 +28,42 @@ export const TitleInvoice = styled.Text`
 `;
 
 export const InvoiceList = styled.ScrollView`
-	max-height: 126px;
+	height: 160px;
 	margin-bottom: 36px;
 `;
 
-export const DateText = styled.Text`
+export const TitleClub = styled.Text`
+	font-size: 16px;
+	font-weight: bold;
 	color: #464646;
-	font-size: 12px;
-	font-weight: 600;
-	${({ normal }) =>
-		normal &&
-		css`
-			font-weight: normal;
-		`}
-`;
-
-export const TitleClub = styled(TitleInvoice)`
 	margin-top: 36px;
-	margin-bottom: 8px;
+	margin-bottom: 12px;
 `;
 
 export const Containerclub = styled.View`
+	padding: 0 16px;
 	width: 100%;
+	margin-bottom: 16px;
 `;
 
-export const Card = styled.View`
-	width: 100%;
-	min-height: 74px;
-	padding: 13px;
-	background: #f0f0f0;
-	border-radius: 12px;
-	align-items: center;
-	flex-direction: row;
-	justify-content: space-between;
-`;
-
-export const TitleCard = styled.Text`
+export const TextCard = styled.Text`
 	font-weight: bold;
-	font-size: 20px;
+	font-size: 22px;
 
-	color: #0098d1;
+	color: #464646;
+	${({ small }) =>
+		small &&
+		css`
+			font-size: 12px;
+		`}
+	${({ secondary }) =>
+		secondary &&
+		css`
+			color: #fff;
+		`}
+	${({ tertiary }) =>
+		tertiary &&
+		css`
+			color: #0098d1;
+		`}
 `;
