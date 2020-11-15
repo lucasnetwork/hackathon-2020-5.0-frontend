0685f00d834e0b006fa3de4acbab6019018211af
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-11-15 14:54:38 -0300

    feat(component of page): add AudioContainer component

---
+++ src/pages/Alerts/components/AudioContainer/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export default styled.View`
+	width: 100%;
+	height: 67px;
+	background: #e1e1e1;
+	border-radius: 60px;
+	flex-direction: row;
+	padding-right: 37px;
+	align-items: flex-start;
+`;
+
+export const ButtonPlay = styled.View`
+	width: 67px;
+	height: 67px;
+	background: #e32119;
+	align-items: center;
+	justify-content: center;
+	border-radius: 50px;
+`;
+
+export const ProgressBarContainer = styled.View`
+	width: 100%;
+	height: 100%;
+	margin-left: 16px;
+	align-items: flex-start;
+	margin-right: 16px;
+	justify-content: center;
+`;
+export const ProgressBar = styled.View`
+	height: 10px;
+	height: 10px;
+	background: #c6c6c6;
+	width: 210px;
+`;
