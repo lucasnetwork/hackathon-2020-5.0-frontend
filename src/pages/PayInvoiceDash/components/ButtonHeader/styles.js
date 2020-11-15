5e559049bd3a449b299ab56cfb41a68cea3e83b4
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-11-15 12:13:20 -0300

    feat(component): add ButtonHeader component of the PayInvoiceDash page

---
+++ src/pages/PayInvoiceDash/components/ButtonHeader/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export default styled.TouchableOpacity`
+	width: 153px;
+	height: 50.64px;
+	border: 1px solid #ffffff;
+	border-radius: 6px;
+	align-items: center;
+	justify-content: center;
+`;
+
+export const Text = styled.Text`
+	font-size: 16px;
+	color: #ffffff;
+`;
