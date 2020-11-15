b6c3ab3f974c598475565d283012f98a859cd0a1
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-11-15 10:06:19 -0300

    feat(component): add Title component

---
+++ src/components/Title/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Title = styled.Text`
+	font-size: 26px;
+	font-weight: bold;
+	color: #582a2a;
+	margin-bottom: 6px;
+`;
+
+export const SubTitle = styled.Text`
+	font-size: 16px;
+	color: #582a2a;
+	opacity: 0.5;
+`;
