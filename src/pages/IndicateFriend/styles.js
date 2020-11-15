e20fe8a4c6ddcb38f8d40029843bc99d230333a0
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-11-15 10:24:13 -0300

    feat(page): create IndicateFriend page

---
+++ src/pages/IndicateFriend/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export default styled.View`
+	flex: 1;
+	padding: 16px;
+	align-items: center;
+	justify-content: center;
+`;
+
+export const ContainerTitle = styled.View`
+	align-self: flex-start;
+	margin-bottom: 20px;
+`;
+
+export const ContainerInput = styled.View`
+	width: 100%;
+	margin: 42px 0;
+`;
