bde81fd99a1cc5c069c863a0bcf6118a606ca809
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-11-14 23:39:37 -0300

    feat(component): add InputText of the ChatBot page

---
+++ src/pages/ChatBot/components/InputText/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export default styled.View`
+	width: 100%;
+	height: 42px;
+	border-radius: 22px;
+	align-items: center;
+	flex-direction: row;
+	padding-left: 16px;
+	padding-right: 16px;
+	background-color: #fff;
+	align-self: flex-end;
+`;
+
+export const Input = styled.TextInput`
+	margin-left: 16px;
+	max-width: 100%;
+	flex: 1;
+`;
